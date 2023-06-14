cd3e44abc6acc814899bab1cdb713963007c80da
twinh <twinhuang@qq.com>
2023-06-14 10:10:50 +0800

    refactor(m): 接口移到 api 目录中

--- tests/pages/api/js-config/IndexTest.php
+++ tests/pages/api/js-config/IndexTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MiaoxingTest\M\Pages\MApi\JsConfig;
+namespace MiaoxingTest\M\Pages\Api\JsConfig;
 
 use Miaoxing\Plugin\Service\Tester;
 use Miaoxing\Plugin\Test\BaseTestCase;
@@ -9,7 +9,7 @@ class IndexTest extends BaseTestCase
 {
     public function testGet()
     {
-        $ret = Tester::get('/m-api/js-config');
+        $ret = Tester::get('/api/js-config');
         $this->assertRetSuc($ret);
         $this->assertArrayHasKey('theme', $ret['data']);
     }
